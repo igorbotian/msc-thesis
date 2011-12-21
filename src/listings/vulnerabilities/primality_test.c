a46e2fbeab496f5f9bd8ec4da36a32f226baa5ea
Igor Botian <igor.botian@gmail.com>
2011-12-21 16:17:11 +0200

    An example of vulnerable program has been moved from embedded listing to a new appendix

--- src/listings/vulnerabilities/primality_test.c
+++ src/listings/vulnerabilities/primality_test.c
@@ -1,18 +1,13 @@
 #include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
-
-/* UCHAR_MAX == 255 */
-#define UCHAR_MAX_LENGTH 3
-
+#define UCHAR_MAX_LENGTH 3 /* UCHAR_MAX == 255 */
 int is_prime(unsigned short number);
 
 int main(int argc, char** argv) {
-    unsigned int number_buf_size = 
-        (UCHAR_MAX_LENGTH + 1);
+    unsigned int number_buf_size = (UCHAR_MAX_LENGTH + 1);
     char* number_buf = 
         (char *) malloc(sizeof(char) * number_buf_size);
-    
     printf("Enter a number (1-%d)", UCHAR_MAX);
     gets(number_buf);
     
@@ -20,24 +15,22 @@ int main(int argc, char** argv) {
         (unsigned char) atoi(number_buf);
     char* result = is_prime(number) 
         ? "is a prime" : "is not a prime";
-    
     printf(number_buf);
     printf(" %s number\n", result);
-    
+
     free(number_buf);
-    
     return EXIT_SUCCESS;
 }
 
 int is_prime(unsigned int number) {
     int result = 1;
     int i;
-    
+
     for(i = 2; i <= number / 2; i++) {
         if((number % i) == 0) {
             result = 0;
         }
     }
-    
+
     return result;
 }
